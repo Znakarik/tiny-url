9b2dc587e61bd04bd746a3217d0dd1a3a26afe2f
olga <znakarik@icloud.com>
2026-01-08 12:22:56 +0300

    Исправлен End2EndTest#testUpdateRedirect()

--- src/main/java/ru/znakarik/controller/analytic/GetUrlDataRq.java
+++ src/main/java/ru/znakarik/controller/analytic/GetUrlDataRq.java
@@ -3,9 +3,11 @@ package ru.znakarik.controller.analytic;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @Getter
-public class GetUrlDataRq {
+public class GetUrlDataRq implements Serializable {
     private final String dateFrom;
     private final String dateTo;
     private final String shortUrl;
--- src/main/java/ru/znakarik/controller/get/single/GetLongUrlRq.java
+++ src/main/java/ru/znakarik/controller/get/single/GetLongUrlRq.java
@@ -2,9 +2,12 @@ package ru.znakarik.controller.get.single;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
+
+import java.io.Serializable;
 
 @AllArgsConstructor
 @Getter
-public class GetLongUrlRq {
+public class GetLongUrlRq implements Serializable {
     private final String shortUrl;
 }
