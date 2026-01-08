9fdd2ad434a89d7f45061e199635b13830006e09
olga <znakarik@icloud.com>
2026-01-08 12:20:44 +0300

    Исправлен End2EndTest#testCreate()

--- src/main/java/ru/znakarik/controller/create/CreateUrlRq.java
+++ src/main/java/ru/znakarik/controller/create/CreateUrlRq.java
@@ -3,8 +3,10 @@ package ru.znakarik.controller.create;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @Getter
-public class CreateUrlRq {
+public class CreateUrlRq implements Serializable {
     private final String longUrl;
 }
